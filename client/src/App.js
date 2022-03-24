352962057ec5b4e2e05d38ad6be20e8952764264
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2022-03-24 12:01:39 -0400

    Add rsuite navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import 'rsuite/dist/rsuite.min.css'
+import './App.css'
+import { Sidenav, Nav } from 'rsuite'
+import { Dashboard } from '@rsuite/icons'
+import { useState } from 'react'
 
 function App() {
+  const [activeKey, setActiveKey] = useState('home')
+  const handleChangeKey = eventKey => {
+    setActiveKey(eventKey)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="nav-wrapper">
+        <Sidenav activeKey={activeKey}>
+          <Sidenav.Body>
+            <Nav>
+              <Nav.Item eventKey="home" icon={<Dashboard />} onSelect={handleChangeKey}>
+                Home
+              </Nav.Item>
+              <Nav.Item eventKey="logs" onSelect={handleChangeKey}>
+                Logs
+              </Nav.Item>
+              <Nav.Item eventKey="traces" onSelect={handleChangeKey}>
+                Traces
+              </Nav.Item>
+            </Nav>
+          </Sidenav.Body>
+        </Sidenav>
+      </div>
+      <main className='main'>
+        <h1>{activeKey}</h1>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
